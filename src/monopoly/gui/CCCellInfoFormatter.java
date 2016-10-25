edd0f7a1992c527ee9bd00a7f479da7c58153f2b
Patryk Kaczmarkiewicz <kaczmarkiewiczp@mymacewan.ca>
2016-10-25 11:40:59 -0600

    Change community chest colour to black

--- src/monopoly/gui/CCCellInfoFormatter.java
+++ src/monopoly/gui/CCCellInfoFormatter.java
@@ -4,6 +4,6 @@ import monopoly.Cell;
 
 public class CCCellInfoFormatter implements CellInfoFormatter {
     public String format(Cell cell) {
-        return "<html><font color='white'><b>" + cell.getName() + "</b></font></html>";
+        return "<html><font color='black'><b>" + cell.getName() + "</b></font></html>";
     }
 }
