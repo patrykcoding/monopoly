dfd08cc3ae5e46f5e56afd3ea9668e46ee1ccd16
Patryk Kaczmarkiewicz <kaczmarkiewiczp@mymacewan.ca>
2016-11-09 22:47:40 -0700

    Remove magic number

--- src/monopoly/Die.java
+++ src/monopoly/Die.java
@@ -1,7 +1,9 @@
 package monopoly;
 
 public class Die {
+    public static final int DICE_SIDES = 6;
+
     public int getRoll() {
-        return (int)(Math.random() * 6) + 1;
+        return (int)(Math.random() * DICE_SIDES) + 1;
     }
 }
