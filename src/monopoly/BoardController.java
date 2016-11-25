80510050f48eb4e0c7238615bc054e46e73c4913
Cody Moorhouse <moorhousec2@mymacewan.ca>
2016-11-25 09:56:53 -0700

    Remove java docs

--- src/monopoly/BoardController.java
+++ src/monopoly/BoardController.java
@@ -24,117 +24,47 @@ public class BoardController {
                                     new Color(72, 196, 188)   /* Player 8 */
                                     ));
 
-    /**
-     * Constructor for the Monopoly BoardController.
-     * 
-     * @param gameBoard GameBoard representing a Monopoly board
-     */
     public BoardController(GameBoard gameBoard) {
         this.gameBoard = gameBoard;
     }
     
-    /**
-     * Returns the current player who has their playerTurnIndex.
-     * 
-     * @return The player who has their playerTurnIndex
-     */
     public Player getCurrentPlayer() {
         return getPlayer(playerTurnIdx);
     }
 
-    /**
-     * Gets the current position index on the gameBoard of the specified player.
-     * 
-     * @param player The player to get the position of
-     * 
-     * @return Returns the index on the gameBoard of the current player
-     */
     public int getCurrentPositionIndex(Player player) {
         Cell currentPosition = player.getPosition();
         return gameBoard.queryCellIndex(currentPosition.getName());
     }
 
-    /**
-     * Gets the associated gameBoard of the boardController.
-     * 
-     * @return Returns a GameBoard representing a monopoly board
-     */
     public GameBoard getGameBoard() {
         return gameBoard;
     }
     
-    /**
-     * Gets a specified player based on their index value.
-     * 
-     * @param index The index of the player ('0' being Player 1)
-     * 
-     * @return Returns the player at the specified index
-     */
     public Player getPlayer(int index) {
         return players.get(index);
     }
 
-    /**
-     * Gets the index of the specified player ('0' being Player 1)
-     * 
-     * @param player A player in the game of monopoly
-     * 
-     * @return Returns the index value of the player
-     */
     public int getPlayerIndex(Player player) {
         return players.indexOf(player);
     }
 
-    /**
-     * Calculates a players new position on the gameBoard by using a position
-     * index and a dice value.
-     * 
-     * @param positionIndex Position index on the gameBoard for a player 
-     * @param diceValue Amount to move the player forward by
-     * 
-     * @return Returns the index for the players new position
-     */
     public int getNewPositionIndex(int positionIndex, int diceValue) {
         return (positionIndex + diceValue) % gameBoard.getCellSize();    
     }
 
-    /**
-     * Returns the number of players for the current game of monopoly on the
-     * gameBoard.
-     * 
-     * @return Amounts of players
-     */
     public int getNumberOfPlayers() {
         return players.size();
     }
     
-    /**
-     * Access method for the players list.
-     * 
-     * @return Returns the List of Players for the current Controller
-     */
     public List<Player> getPlayers() {
         return players;
     }
     
-    /**
-     * Returns the index of the player who's turn it currently is. ('0' is
-     * Player 1)
-     * 
-     * @return Returns the current players index
-     */
     public int getTurn() {
         return playerTurnIdx;
     }
 	
-    /**
-     * Moves the specified player by the diceValue forward on the gameBoard. 
-     * This will not set the players position, but instead just advance them 
-     * from their current position.
-     * 
-     * @param player Player to move on the gameBoard
-     * @param diceValue Amount to move the player by
-     */
     public void movePlayer(Player player, int diceValue) {
         int positionIndex = getCurrentPositionIndex(player);
         int newIndex = getNewPositionIndex(positionIndex, diceValue);
@@ -144,12 +74,6 @@ public class BoardController {
         player.setPosition(gameBoard.getCell(newIndex));
     }
     
-    /**
-     * Resets the controller by assigning all the current players back onto
-     * the first cell of the gameBoard and resetting the playerTurnIdx. This
-     * will not change the current state of the player (e.g. money, name, color,
-     * etc)
-     */
     public void reset() {    
         for (int i = 0; i < getNumberOfPlayers(); i++) {
             Player player = players.get(i);
@@ -158,22 +82,10 @@ public class BoardController {
         playerTurnIdx = 0;
     }
 
-    /**
-     * Sets a new gameBoard to the controller.
-     * 
-     * @param board A GameBoard representing a Monopoly board
-     */
     public void setGameBoard(GameBoard board) {
         this.gameBoard = board;
     }
     
-    /**
-     * Sets the number of players on the gameBoard. This will clear any previous
-     * players on the gameBoard. Players are automatically assigned to start on 
-     * the first cell on the gameBoard.
-     * 
-     * @param number Number of players
-     */
     public void setNumberOfPlayers(int number) {
         players.clear();
         for (int i = 0; i < number; i++) {
@@ -183,9 +95,6 @@ public class BoardController {
         }
     }
     
-    /**
-     * Switches to the next players playerTurnIndex. (Goes in order)
-     */
     public void switchTurn() {
         playerTurnIdx = (playerTurnIdx + 1) % getNumberOfPlayers();
     }
